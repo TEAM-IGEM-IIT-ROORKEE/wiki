2e9c3fdb43f7418f44efe2d014efa58509965939
Kushagra Agarwal <agarwal.kush17181@gmail.com>
2020-04-25 00:38:40 +0530

    added active on navbar

--- js/button.js
+++
@@ -1,5 +0,0 @@
-function getstarted(){
-    $("html, body").animate({
-        scrollTop: $("#aboutigem").offset().top - 0.2*$(window).width()    
-    }, 2000);
-};
---
+++ js/script.js
@@ -0,0 +1,36 @@
+function getstarted(){
+    $("html, body").animate({
+        scrollTop: $("#aboutigem").offset().top - 0.2*$(window).width()    
+    }, 2000);
+};
+function team(){
+    $("html, body").animate({
+        scrollTop: $("#team").offset().top - 0.2*$(window).width()    
+    }, 2000);
+};
+function problem(){
+    $("html, body").animate({
+        scrollTop: $("#problem").offset().top - 0.2*$(window).width()    
+    }, 2000);
+};
+function home(){
+    $("html, body").animate({
+        scrollTop: 0    
+    }, 2000);
+};
+
+
+window.onscroll=function(){navActive()};
+
+function navActive(){
+    x=document.getElementsByClassName('target');
+    y=document.getElementsByClassName('nav-item');
+    for(i=0;i<x.length;i++){
+        if((x[i].getBoundingClientRect().top<window.innerHeight/2)&&(x[i].getBoundingClientRect().bottom>window.innerHeight/2)){
+            y[i].classList.add('active');
+        }
+        else{
+            y[i].classList.remove('active');
+        }
+    }
+}
